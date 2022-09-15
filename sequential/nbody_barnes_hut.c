1ff840c288cd942eff7968418b8c93ac4e4cb6c8
Osgood <eloiposeidon@gmail.com>
2022-09-15 10:07:24 +0200

    format issue

--- sequential/nbody_barnes_hut.c
+++ sequential/nbody_barnes_hut.c
@@ -20,55 +20,59 @@
 #include "nbody.h"
 #include "nbody_tools.h"
 
-FILE* f_out=NULL;
+FILE *f_out = NULL;
 
-int nparticles=10;      /* number of particles */
-float T_FINAL=1.0;     /* simulation end time */
+int nparticles = 1000; /* number of particles */
+float T_FINAL = 1.0;   /* simulation end time */
 
-particle_t*particles;
+particle_t *particles;
 
 node_t *root;
-
 
 double sum_speed_sq = 0;
 double max_acc = 0;
 double max_speed = 0;
 
-void init() {
-  init_alloc(8*nparticles);
+void init()
+{
+  init_alloc(8 * nparticles);
   root = malloc(sizeof(node_t));
   init_node(root, NULL, XMIN, XMAX, YMIN, YMAX);
 }
 
 #ifdef DISPLAY
-Display *theDisplay;  /* These three variables are required to open the */
-GC theGC;             /* particle plotting window.  They are externally */
-Window theMain;       /* declared in ui.h but are also required here.   */
+Display *theDisplay; /* These three variables are required to open the */
+GC theGC;            /* particle plotting window.  They are externally */
+Window theMain;      /* declared in ui.h but are also required here.   */
 #endif
 
 /* compute the force that a particle with position (x_pos, y_pos) and mass 'mass'
  * applies to particle p
  */
-void compute_force(particle_t*p, double x_pos, double y_pos, double mass) {
+void compute_force(particle_t *p, double x_pos, double y_pos, double mass)
+{
   double x_sep, y_sep, dist_sq, grav_base;
 
   x_sep = x_pos - p->x_pos;
   y_sep = y_pos - p->y_pos;
-  dist_sq = MAX((x_sep*x_sep) + (y_sep*y_sep), 0.01);
+  dist_sq = MAX((x_sep * x_sep) + (y_sep * y_sep), 0.01);
 
   /* Use the 2-dimensional gravity rule: F = d * (GMm/d^2) */
-  grav_base = GRAV_CONSTANT*(p->mass)*(mass)/dist_sq;
+  grav_base = GRAV_CONSTANT * (p->mass) * (mass) / dist_sq;
 
-  p->x_force += grav_base*x_sep;
-  p->y_force += grav_base*y_sep;
+  p->x_force += grav_base * x_sep;
+  p->y_force += grav_base * y_sep;
 }
 
 /* compute the force that node n acts on particle p */
-void compute_force_on_particle(node_t* n, particle_t *p) {
-  if(! n || n->n_particles==0) {
+void compute_force_on_particle(node_t *n, particle_t *p)
+{
+  if (!n || n->n_particles == 0)
+  {
     return;
   }
-  if(n->particle) {
+  if (n->particle)
+  {
     /* only one particle */
     assert(n->children == NULL);
 
@@ -78,14 +82,16 @@ void compute_force_on_particle(node_t* n, particle_t *p) {
       this amount to b's net force.
     */
     compute_force(p, n->x_center, n->y_center, n->mass);
-  } else {
+  }
+  else
+  {
     /* There are multiple particles */
 
-    #define THRESHOLD 2
+#define THRESHOLD 2
     double size = n->x_max - n->x_min; // width of n
     double diff_x = n->x_center - p->x_pos;
     double diff_y = n->y_center - p->y_pos;
-    double distance = sqrt(diff_x*diff_x + diff_y*diff_y);
+    double distance = sqrt(diff_x * diff_x + diff_y * diff_y);
 
 #if BRUTE_FORCE
     /*
@@ -94,62 +100,73 @@ void compute_force_on_particle(node_t* n, particle_t *p) {
       --> This result in a brute-force computation (complexity: O(n*n))
     */
     int i;
-    for(i=0; i<4; i++) {
+    for (i = 0; i < 4; i++)
+    {
       compute_force_on_particle(&n->children[i], p);
     }
 #else
     /* Use the Barnes-Hut algorithm to get an approximation */
-    if(size / distance < THRESHOLD) {
+    if (size / distance < THRESHOLD)
+    {
       /*
-	The particle is far away. Use an approximation of the force
+  The particle is far away. Use an approximation of the force
       */
       compute_force(p, n->x_center, n->y_center, n->mass);
-    } else {
+    }
+    else
+    {
       /*
         Otherwise, run the procedure recursively on each of the current
-	node's children.
+  node's children.
       */
       int i;
-      for(i=0; i<4; i++) {
-	compute_force_on_particle(&n->children[i], p);
+      for (i = 0; i < 4; i++)
+      {
+        compute_force_on_particle(&n->children[i], p);
       }
     }
 #endif
   }
 }
 
-void compute_force_in_node(node_t *n) {
-  if(!n) return;
+void compute_force_in_node(node_t *n)
+{
+  if (!n)
+    return;
 
-  if(n->particle) {
-    particle_t*p = n->particle;
+  if (n->particle)
+  {
+    particle_t *p = n->particle;
     p->x_force = 0;
     p->y_force = 0;
     compute_force_on_particle(root, p);
   }
-  if(n->children) {
+  if (n->children)
+  {
     int i;
-    for(i=0; i<4; i++) {
+    for (i = 0; i < 4; i++)
+    {
       compute_force_in_node(&n->children[i]);
     }
   }
 }
 
 /* compute the new position/velocity */
-void move_particle(particle_t*p, double step, node_t* new_root) {
+void move_particle(particle_t *p, double step, node_t *new_root)
+{
 
   assert(p->node != NULL);
-  p->x_pos += (p->x_vel)*step;
-  p->y_pos += (p->y_vel)*step;
-  double x_acc = p->x_force/p->mass;
-  double y_acc = p->y_force/p->mass;
-  p->x_vel += x_acc*step;
-  p->y_vel += y_acc*step;
+  p->x_pos += (p->x_vel) * step;
+  p->y_pos += (p->y_vel) * step;
+  double x_acc = p->x_force / p->mass;
+  double y_acc = p->y_force / p->mass;
+  p->x_vel += x_acc * step;
+  p->y_vel += y_acc * step;
 
   /* compute statistics */
-  double cur_acc = (x_acc*x_acc + y_acc*y_acc);
+  double cur_acc = (x_acc * x_acc + y_acc * y_acc);
   cur_acc = sqrt(cur_acc);
-  double speed_sq = (p->x_vel)*(p->x_vel) + (p->y_vel)*(p->y_vel);
+  double speed_sq = (p->x_vel) * (p->x_vel) + (p->y_vel) * (p->y_vel);
   double cur_speed = sqrt(speed_sq);
 
   sum_speed_sq += speed_sq;
@@ -157,27 +174,35 @@ void move_particle(particle_t*p, double step, node_t* new_root) {
   max_speed = MAX(max_speed, cur_speed);
 
   p->node = NULL;
-  if(p->x_pos < new_root->x_min ||
-     p->x_pos > new_root->x_max ||
-     p->y_pos < new_root->y_min ||
-     p->y_pos > new_root->y_max) {
+  if (p->x_pos < new_root->x_min ||
+      p->x_pos > new_root->x_max ||
+      p->y_pos < new_root->y_min ||
+      p->y_pos > new_root->y_max)
+  {
     nparticles--;
-  } else {
+  }
+  else
+  {
     insert_particle(p, new_root);
   }
 }
 
 /* compute the new position of the particles in a node */
-void move_particles_in_node(node_t*n, double step, node_t *new_root) {
-  if(!n) return;
+void move_particles_in_node(node_t *n, double step, node_t *new_root)
+{
+  if (!n)
+    return;
 
-  if(n->particle) {
-    particle_t*p = n->particle;
+  if (n->particle)
+  {
+    particle_t *p = n->particle;
     move_particle(p, step, new_root);
   }
-  if(n->children) {
+  if (n->children)
+  {
     int i;
-    for(i=0; i<4; i++) {
+    for (i = 0; i < 4; i++)
+    {
       move_particles_in_node(&n->children[i], step, new_root);
     }
   }
@@ -194,7 +219,7 @@ void all_move_particles(double step)
   /* First calculate force for particles. */
   compute_force_in_node(root);
 
-  node_t* new_root = alloc_node();
+  node_t *new_root = alloc_node();
   init_node(new_root, NULL, XMIN, XMAX, YMIN, YMAX);
 
   /* then move all particles and return statistics */
@@ -204,10 +229,12 @@ void all_move_particles(double step)
   root = new_root;
 }
 
-void run_simulation() {
+void run_simulation()
+{
   double t = 0.0, dt = 0.01;
 
-  while (t < T_FINAL && nparticles>0) {
+  while (t < T_FINAL && nparticles > 0)
+  {
     /* Update time. */
     t += dt;
     /* Move particles with the current and compute rms velocity. */
@@ -217,7 +244,7 @@ void run_simulation() {
        simple rule tries to insure that no velocity will change
        by more than 10% */
 
-    dt = 0.1*max_speed/max_acc;
+    dt = 0.1 * max_speed / max_acc;
 
     /* Plot the movement of the particle */
 #if DISPLAY
@@ -230,9 +257,11 @@ void run_simulation() {
 }
 
 /* create a quad-tree from an array of particles */
-void insert_all_particles(int nparticles, particle_t*particles, node_t*root) {
+void insert_all_particles(int nparticles, particle_t *particles, node_t *root)
+{
   int i;
-  for(i=0; i<nparticles; i++) {
+  for (i = 0; i < nparticles; i++)
+  {
     insert_particle(&particles[i], root);
   }
 }
@@ -240,26 +269,28 @@ void insert_all_particles(int nparticles, particle_t*particles, node_t*root) {
 /*
   Simulate the movement of nparticles particles.
 */
-int main(int argc, char**argv)
+int main(int argc, char **argv)
 {
-  if(argc >= 2) {
+  if (argc >= 2)
+  {
     nparticles = atoi(argv[1]);
   }
-  if(argc == 3) {
+  if (argc == 3)
+  {
     T_FINAL = atof(argv[2]);
   }
 
   init();
 
   /* Allocate global shared arrays for the particles data set. */
-  particles = malloc(sizeof(particle_t)*nparticles);
+  particles = malloc(sizeof(particle_t) * nparticles);
   all_init_particles(nparticles, particles);
   insert_all_particles(nparticles, particles, root);
 
   /* Initialize thread data structures */
 #ifdef DISPLAY
   /* Open an X window to display the particles */
-  simple_init (100,100,DISPLAY_SIZE, DISPLAY_SIZE);
+  simple_init(100, 100, DISPLAY_SIZE, DISPLAY_SIZE);
 #endif
 
   struct timeval t1, t2;
@@ -270,10 +301,10 @@ int main(int argc, char**argv)
 
   gettimeofday(&t2, NULL);
 
-  double duration = (t2.tv_sec -t1.tv_sec)+((t2.tv_usec-t1.tv_usec)/1e6);
+  double duration = (t2.tv_sec - t1.tv_sec) + ((t2.tv_usec - t1.tv_usec) / 1e6);
 
 #ifdef DUMP_RESULT
-  FILE* f_out = fopen("particles.log", "w");
+  FILE *f_out = fopen("particles.log", "w");
   assert(f_out);
   print_particles(f_out, root);
   fclose(f_out);
--- sequential/nbody_brute_force.c
+++ sequential/nbody_brute_force.c
@@ -20,64 +20,66 @@
 #include "nbody.h"
 #include "nbody_tools.h"
 
-FILE* f_out=NULL;
+FILE *f_out = NULL;
 
-int nparticles=10;      /* number of particles */
-float T_FINAL=1.0;     /* simulation end time */
-particle_t*particles;
+int nparticles = 1500; /* number of particles */
+float T_FINAL = 1.0;   /* simulation end time */
+particle_t *particles;
 
 double sum_speed_sq = 0;
 double max_acc = 0;
 double max_speed = 0;
 
-void init() {
+void init()
+{
   /* Nothing to do */
 }
 
 #ifdef DISPLAY
-Display *theDisplay;  /* These three variables are required to open the */
-GC theGC;             /* particle plotting window.  They are externally */
-Window theMain;       /* declared in ui.h but are also required here.   */
+Display *theDisplay; /* These three variables are required to open the */
+GC theGC;            /* particle plotting window.  They are externally */
+Window theMain;      /* declared in ui.h but are also required here.   */
 #endif
 
 /* compute the force that a particle with position (x_pos, y_pos) and mass 'mass'
  * applies to particle p
  */
-void compute_force(particle_t*p, double x_pos, double y_pos, double mass) {
+void compute_force(particle_t *p, double x_pos, double y_pos, double mass)
+{
   double x_sep, y_sep, dist_sq, grav_base;
 
   x_sep = x_pos - p->x_pos;
   y_sep = y_pos - p->y_pos;
-  dist_sq = MAX((x_sep*x_sep) + (y_sep*y_sep), 0.01);
+  dist_sq = MAX((x_sep * x_sep) + (y_sep * y_sep), 0.01);
 
   /* Use the 2-dimensional gravity rule: F = d * (GMm/d^2) */
-  grav_base = GRAV_CONSTANT*(p->mass)*(mass)/dist_sq;
+  grav_base = GRAV_CONSTANT * (p->mass) * (mass) / dist_sq;
 
-  p->x_force += grav_base*x_sep;
-  p->y_force += grav_base*y_sep;
+  p->x_force += grav_base * x_sep;
+  p->y_force += grav_base * y_sep;
 }
 
 /* compute the new position/velocity */
-void move_particle(particle_t*p, double step) {
+void move_particle(particle_t *p, double step)
+{
 
-  p->x_pos += (p->x_vel)*step;
-  p->y_pos += (p->y_vel)*step;
-  double x_acc = p->x_force/p->mass;
-  double y_acc = p->y_force/p->mass;
-  p->x_vel += x_acc*step;
-  p->y_vel += y_acc*step;
+  p->x_pos += (p->x_vel) * step;
+  p->y_pos += (p->y_vel) * step;
+  double x_acc = p->x_force / p->mass;
+  double y_acc = p->y_force / p->mass;
+  p->x_vel += x_acc * step;
+  p->y_vel += y_acc * step;
 
   /* compute statistics */
-  double cur_acc = (x_acc*x_acc + y_acc*y_acc);
+  double cur_acc = (x_acc * x_acc + y_acc * y_acc);
   cur_acc = sqrt(cur_acc);
-  double speed_sq = (p->x_vel)*(p->x_vel) + (p->y_vel)*(p->y_vel);
+  double speed_sq = (p->x_vel) * (p->x_vel) + (p->y_vel) * (p->y_vel);
   double cur_speed = sqrt(speed_sq);
 
   sum_speed_sq += speed_sq;
   max_acc = MAX(max_acc, cur_acc);
   max_speed = MAX(max_speed, cur_speed);
 }
-
 
 /*
   Move particles one time step.
@@ -89,44 +91,53 @@ void all_move_particles(double step)
 {
   /* First calculate force for particles. */
   int i;
-  for(i=0; i<nparticles; i++) {
+  for (i = 0; i < nparticles; i++)
+  {
     int j;
     particles[i].x_force = 0;
     particles[i].y_force = 0;
-    for(j=0; j<nparticles; j++) {
-      particle_t*p = &particles[j];
+    for (j = 0; j < nparticles; j++)
+    {
+      particle_t *p = &particles[j];
       /* compute the force of particle j on particle i */
       compute_force(&particles[i], p->x_pos, p->y_pos, p->mass);
     }
   }
 
   /* then move all particles and return statistics */
-  for(i=0; i<nparticles; i++) {
+  for (i = 0; i < nparticles; i++)
+  {
     move_particle(&particles[i], step);
   }
 }
 
 /* display all the particles */
-void draw_all_particles() {
+void draw_all_particles()
+{
   int i;
-  for(i=0; i<nparticles; i++) {
+  for (i = 0; i < nparticles; i++)
+  {
     int x = POS_TO_SCREEN(particles[i].x_pos);
     int y = POS_TO_SCREEN(particles[i].y_pos);
-    draw_point (x,y);
+    draw_point(x, y);
   }
 }
 
-void print_all_particles(FILE* f) {
+void print_all_particles(FILE *f)
+{
   int i;
-  for(i=0; i<nparticles; i++) {
-    particle_t*p = &particles[i];
+  for (i = 0; i < nparticles; i++)
+  {
+    particle_t *p = &particles[i];
     fprintf(f, "particle={pos=(%f,%f), vel=(%f,%f)}\n", p->x_pos, p->y_pos, p->x_vel, p->y_vel);
   }
 }
 
-void run_simulation() {
+void run_simulation()
+{
   double t = 0.0, dt = 0.01;
-  while (t < T_FINAL && nparticles>0) {
+  while (t < T_FINAL && nparticles > 0)
+  {
     /* Update time. */
     t += dt;
     /* Move particles with the current and compute rms velocity. */
@@ -136,7 +147,7 @@ void run_simulation() {
        simple rule tries to insure that no velocity will change
        by more than 10% */
 
-    dt = 0.1*max_speed/max_acc;
+    dt = 0.1 * max_speed / max_acc;
 
     /* Plot the movement of the particle */
 #if DISPLAY
@@ -150,25 +161,27 @@ void run_simulation() {
 /*
   Simulate the movement of nparticles particles.
 */
-int main(int argc, char**argv)
+int main(int argc, char **argv)
 {
-  if(argc >= 2) {
+  if (argc >= 2)
+  {
     nparticles = atoi(argv[1]);
   }
-  if(argc == 3) {
+  if (argc == 3)
+  {
     T_FINAL = atof(argv[2]);
   }
 
   init();
 
   /* Allocate global shared arrays for the particles data set. */
-  particles = malloc(sizeof(particle_t)*nparticles);
+  particles = malloc(sizeof(particle_t) * nparticles);
   all_init_particles(nparticles, particles);
 
   /* Initialize thread data structures */
 #ifdef DISPLAY
   /* Open an X window to display the particles */
-  simple_init (100,100,DISPLAY_SIZE, DISPLAY_SIZE);
+  simple_init(100, 100, DISPLAY_SIZE, DISPLAY_SIZE);
 #endif
 
   struct timeval t1, t2;
@@ -179,10 +192,10 @@ int main(int argc, char**argv)
 
   gettimeofday(&t2, NULL);
 
-  double duration = (t2.tv_sec -t1.tv_sec)+((t2.tv_usec-t1.tv_usec)/1e6);
+  double duration = (t2.tv_sec - t1.tv_sec) + ((t2.tv_usec - t1.tv_usec) / 1e6);
 
 #ifdef DUMP_RESULT
-  FILE* f_out = fopen("particles.log", "w");
+  FILE *f_out = fopen("particles.log", "w");
   assert(f_out);
   print_all_particles(f_out);
   fclose(f_out);
