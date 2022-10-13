f8a5e74e055d1093073f70d0492210910040a65a
lebermuda <jean35lb@gmail.com>
2022-10-13 15:39:35 +0200

    rm Cuda g++

--- CUDAg++/xstuff.h
+++
@@ -1,9 +0,0 @@
-#ifndef XSTUFF_H
-#define XSTUFF_H
-
-void initapp(int argc, char **argv, int x, int y, 
-	     unsigned width, unsigned height);
-void usage(void);
-void xwin_init(void);
-
-#endif /* XSTUFF_H */
