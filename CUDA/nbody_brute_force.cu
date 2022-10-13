45d1e6e733c983d0ef64fde2c83f0a964c2bffab
lebermuda <jean35lb@gmail.com>
2022-10-13 15:42:57 +0200

    CUDA

--- CUDA/nbody_brute_force.cu
+++ CUDA/nbody_brute_force.cu
@@ -313,6 +313,33 @@ void run_simulation()
 }
 
 /*
+  Place particles in their initial positions.
+*/
+void all_init_particles(int num_particles, particle_t *particles)
+{
+    int    i;
+    double total_particle = num_particles;
+
+    for (i = 0; i < num_particles; i++) {
+        particle_t *particle = &particles[i];
+#if 0
+        particle->x_pos = ((rand() % max_resolution)- (max_resolution/2))*2.0 / max_resolution;
+    particle->y_pos = ((rand() % max_resolution)- (max_resolution/2))*2.0 / max_resolution;
+    particle->x_vel = particle->y_pos;
+    particle->y_vel = particle->x_pos;
+#else
+        particle->x_pos = i*2.0/nparticles - 1.0;
+        particle->y_pos = 0.0;
+        particle->x_vel = 0.0;
+        particle->y_vel = particle->x_pos;
+#endif
+        particle->mass = 1.0 + (num_particles+i)/total_particle;
+        particle->node = NULL;
+
+        //insert_particle(particle, root);
+    }
+}
+/*
   Simulate the movement of nparticles particles.
 */
 int main(int argc, char **argv)
